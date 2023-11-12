ee1e971c067dc5960eef473df8fbc4922501749e
Igor Suvorov <hi@isuvorov.com>
2023-11-12 20:17:12 +0300

    feat: new errors

--- libs/err/src/index.ts
+++ libs/err/src/index.ts
@@ -1,6 +1,6 @@
 export * from './Err.js';
 export * from './errMerge.js';
 export * from './errProps.js';
-export * from './utils.js';
 export * from './types.js';
+export * from './utils.js';
 export { default } from './Err.js';

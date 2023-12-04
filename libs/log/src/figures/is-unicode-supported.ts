1b3b19f12b48abc3b141e2da64ecdb8fc2043023
Igor Suvorov <hi@isuvorov.com>
2023-12-04 19:21:25 +0300

    fix: figures works in browsers

--- libs/log/src/figures/is-unicode-supported.ts
+++ libs/log/src/figures/is-unicode-supported.ts
@@ -1,19 +1,20 @@
-import process from 'node:process';
+// import process from 'node:process';
 
 export function isUnicodeSupported() {
-  if (process.platform !== 'win32') {
-    return process.env.TERM !== 'linux'; // Linux console (kernel)
+  const proc = typeof process !== 'undefined' ? process : ({} as any);
+  if (proc?.platform !== 'win32') {
+    return proc?.env?.TERM !== 'linux'; // Linux console (kernel)
   }
 
   return (
-    Boolean(process.env.WT_SESSION) || // Windows Terminal
-    Boolean(process.env.TERMINUS_SUBLIME) || // Terminus (<0.2.27)
-    process.env.ConEmuTask === '{cmd::Cmder}' || // ConEmu and cmder
-    process.env.TERM_PROGRAM === 'Terminus-Sublime' ||
-    process.env.TERM_PROGRAM === 'vscode' ||
-    process.env.TERM === 'xterm-256color' ||
-    process.env.TERM === 'alacritty' ||
-    process.env.TERMINAL_EMULATOR === 'JetBrains-JediTerm'
+    Boolean(proc?.env?.WT_SESSION) || // Windows Terminal
+    Boolean(proc?.env?.TERMINUS_SUBLIME) || // Terminus (<0.2.27)
+    proc?.env?.ConEmuTask === '{cmd::Cmder}' || // ConEmu and cmder
+    proc?.env?.TERM_PROGRAM === 'Terminus-Sublime' ||
+    proc?.env?.TERM_PROGRAM === 'vscode' ||
+    proc?.env?.TERM === 'xterm-256color' ||
+    proc?.env?.TERM === 'alacritty' ||
+    proc?.env?.TERMINAL_EMULATOR === 'JetBrains-JediTerm'
   );
 }
 

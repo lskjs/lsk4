20344a0127d451237742a5749fcefb36a2598dc8
Igor Suvorov <hi@isuvorov.com>
2023-12-25 21:16:59 +0300

    feat(env): improve process env safety for storybook

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -1,32 +1,26 @@
-/* global window */
-
-// import { getEnvVar } from './getEnvVar';
-
 // @ts-ignore
 const safeWindow: any = typeof window !== 'undefined' ? window : null;
+const safeProcess: any = typeof process !== 'undefined' ? process : null;
+const env = safeProcess?.env || safeWindow?.env || {};
 
 export const isServer = !safeWindow;
 export const isClient = !isServer;
-export const isDev = isServer
-  ? process.env.NODE_ENV !== 'production'
-  : Boolean(safeWindow?.__DEV__);
+export const isDev = isServer ? env.NODE_ENV !== 'production' : Boolean(safeWindow?.__DEV__);
 export const isProd = !isDev;
 export const isDebug = isDev;
-export const isTTY = Boolean(process?.stdout?.isTTY) || false;
-
+export const isTTY = Boolean(safeProcess?.stdout?.isTTY) || false;
 // getEnvVar('STAGE', (isDev ? process.env.user : null))
 
 export const stage =
   (isServer
-    ? process.env.STAGE || (isDev ? process.env.user : null)
+    ? env.STAGE || (isDev ? env.user : null)
     : safeWindow?.env?.stage || safeWindow.__STAGE__) || 'development';
 
 export const version =
   (isServer
-    ? process.env.VERSION || process.env.CI_COMMIT_SHORT_SHA || process.env.CI_COMMIT_SHA
+    ? env.VERSION || env.CI_COMMIT_SHORT_SHA || env.CI_COMMIT_SHA
     : safeWindow?.env?.version) || stage;
 
-const env = isServer ? process.env : {};
 export const isCI =
   isServer &&
   env.CI !== 'false' &&

5e1e0d6b4c451cd99a1a4907f4075170db255363
Igor Suvorov <hi@isuvorov.com>
2024-08-31 15:40:18 +0300

    fix(algos): improve non nulable type

--- libs/algos/src/omitNull.ts
+++ libs/algos/src/omitNull.ts
@@ -1,8 +1,56 @@
 import { pickBy } from './pickBy.js';
+// import { NonNullable } from './types.js';
 
 export const omitNull = <T extends Record<string, any>>(
   obj: T,
   filter: (value: T[keyof T]) => boolean = (a) => a != null,
-): Partial<T> => pickBy(obj, filter);
+) => pickBy(obj, filter) as Partial<NonNullable<T>>;
 
-export default omitNull;
+// type OmitNullAndUndefined<T> = {
+//   [K in keyof T]: Exclude<T[K], null | undefined>;
+// };
+
+// type NonNullableKeys<T> = {
+//   [K in keyof T as T[K] extends null | undefined ? never : K]: K;
+// }[keyof T];
+
+// type FilteredResult<T> = Pick<OmitNullAndUndefined<T>, NonNullableKeys<T>>;
+
+// export const omitNull = <T extends Record<string, any>>(
+//   obj: T,
+//   filter: (value: T[keyof T]) => boolean = (a) => a != null,
+// ): FilteredResult<T> => pickBy(obj, filter) as FilteredResult<T>;
+
+// type OmitNullAndUndefined<T> = {
+//   [K in keyof T as T[K] extends null | undefined ? never : K]: Exclude<T[K], null | undefined>;
+// };
+// const b = omitNull(a, (a) => a != null) as OmitNullAndUndefined<typeof a>;
+// const c =  omitNull(a);
+// const g = {
+//   q: c.a
+// }
+
+// const a = { a: 1, b: null, c: 3 };
+
+// // const z = a as NonNullable<typeof a>;
+// const z = a as Omit<typeof a, 'b'> ;
+
+// const za = z.a
+// const zb = z.b
+// const zc = z.c
+
+// type NonNullable2<T> = Exclude<T, null | undefined>;
+// const z2 = a as NonNullable2<typeof a>;
+// const z2a = z2.a
+// const z2b = z2.b
+// const z2c = z2.c
+
+// type NonNullable1<T> = {
+//   [P in keyof T]-?: Exclude<T[P], null | undefined>;
+// };
+// const z1 = a as NonNullable1<typeof a>;
+// const z1a = z1.a
+// const z1b = z1.b
+// const z1c = z1.c
+
+// export default omitNull;
--- libs/algos/src/types.ts
+++ libs/algos/src/types.ts
@@ -1,13 +1,30 @@
-export type ObjectLike<T> = Record<string, T>;
-export type ObjectKeyPath = string | string[];
-export type ObjectFilter<T> = (value?: T, key?: string, object?: ObjectLike<T>) => boolean;
-export type ObjectMapper<T> = (value?: T, key?: string, object?: ObjectLike<T>) => any;
+// export type ObjectLike<T> = Record<string, T>;
+// export type ObjectKeyPath = string | string[];
+// export type ObjectFilter<T> = (value?: T, key?: string, object?: ObjectLike<T>) => boolean;
+// export type ObjectMapper<T> = (value?: T, key?: string, object?: ObjectLike<T>) => any;
 
-export interface PickOmit {
-  <T extends Record<string, unknown>, K extends [...(keyof T)[]]>(
-    obj: T,
-    keys: K,
-  ): {
-    [K2 in Exclude<keyof T, K[number]>]: T[K2];
-  };
-}
+// export interface PickOmit {
+//   <T extends Record<string, unknown>, K extends [...(keyof T)[]]>(
+//     obj: T,
+//     keys: K,
+//   ): {
+//     [K2 in Exclude<keyof T, K[number]>]: T[K2];
+//   };
+// }
+
+
+
+
+// export type NonNullable<T> = {
+//   [K in keyof T]: Exclude<T[K], null | undefined>;
+// };
+
+
+// export type NonNullable<T> = Exclude<T, null | undefined>;
+// export type NonNullable<T> = {
+//   [P in keyof T]-?: Exclude<T[P], null | undefined>;
+// };
+
+export type NonNullable1<T> = {
+  [P in keyof T]-?: Exclude<T[P], null | undefined>;
+};

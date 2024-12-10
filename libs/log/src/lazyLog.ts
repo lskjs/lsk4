5a04aa9b4d58bb6f8fd675d12a49d19bfddb78a0
Igor Suvorov <hi@isuvorov.com>
2024-12-10 14:46:37 +0300

    feat(log): improve lazyLog

--- libs/log/src/lazyLog.ts
+++ libs/log/src/lazyLog.ts
@@ -1,8 +1,28 @@
 import { createLogger } from './createLogger.js';
 import type { ILogger } from './types.js';
 
-const loggers: Record<string, ILogger> = {};
-export const lazyLog = (ns: string = '') => {
-  if (!loggers[ns]) loggers[ns] = createLogger(ns);
-  return loggers[ns];
-};
+interface LazyLogFunction {
+  (ns?: string): ILogger;
+  loggers: Record<string, ILogger>;
+  trace: ILogger['trace'];
+  debug: ILogger['debug'];
+  info: ILogger['info'];
+  warn: ILogger['warn'];
+  error: ILogger['error'];
+  fatal: ILogger['fatal'];
+}
+
+const lazyLoggers: Record<string, ILogger> = {};
+export const lazyLog = function (ns: string = ''): ILogger {
+  if (!lazyLoggers[ns]) lazyLoggers[ns] = createLogger(ns);
+  return lazyLoggers[ns];
+} as LazyLogFunction;
+
+lazyLog.loggers = lazyLoggers;
+const lazy = lazyLog();
+lazyLog.trace = lazy.trace.bind(lazy);
+lazyLog.debug = lazy.debug.bind(lazy);
+lazyLog.info = lazy.info.bind(lazy);
+lazyLog.warn = lazy.warn.bind(lazy);
+lazyLog.error = lazy.error.bind(lazy);
+lazyLog.fatal = lazy.fatal.bind(lazy);

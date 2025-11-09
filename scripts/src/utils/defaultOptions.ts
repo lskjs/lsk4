f4f88b6dd2ae62bf93c9dec9d1ea4e648458a108
isuvorov <hi@isuvorov.com>
2025-11-09 14:35:55 +0300

    feat: prepare for release

--- scripts/src/utils/defaultOptions.ts
+++ scripts/src/utils/defaultOptions.ts
@@ -1,9 +1,9 @@
 import { isCI, isDev } from 'ycmd';
 
 export const defaultOptions = {
-  isSilent: !!+process.env.YCMD_SILENT || isCI,
+  isSilent: !!+process.env.YCMD_SILENT! || isCI,
   isBail: isCI,
-  isProd: !!+process.env.YCMD_PROD || !isDev,
+  isProd: !!+process.env.YCMD_PROD! || !isDev,
   isCjs: true,
   libDir: 'lib',
   cjsDir: 'cjs',
--- scripts/src/utils/writeFileAndDir.ts
+++ scripts/src/utils/writeFileAndDir.ts
@@ -1,7 +1,7 @@
 import { mkdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
-export const writeFileAndDir = async (path, content) => {
+export const writeFileAndDir = async (path: string, content: string) => {
   const dir = join(path, '..');
   await mkdir(dir, { recursive: true });
   await writeFile(path, content);
